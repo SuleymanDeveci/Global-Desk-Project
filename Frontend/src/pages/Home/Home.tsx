a893f427fbf892933aa30c3f9d324225eb140c31
Süleyman Deveci <suleymandeveci1907@gmail.com>
2024-12-22 04:55:57 +0200

    Home.tsx and Home.css created
    Home page created and fetchClients code added

--- Frontend/src/pages/Home/Home.tsx
+++ Frontend/src/pages/Home/Home.tsx
@@ -1,5 +1,56 @@
+import React, { useEffect, useState } from "react";
+
+type Client = {
+  name: string;
+  nationality: string;
+  occupation: string;
+  email: string;
+};
+
 const Home: React.FC = () => {
-  return <div>Home Page</div>;
+  const [clients, setClients] = useState<Client[]>([]);
+
+  useEffect(() => {
+    const fetchClients = async () => {
+      try {
+        const response = await fetch("http://localhost:7046/api/clients");
+        if (!response.ok) {
+          throw new Error("Failed to fetch clients");
+        }
+        const data = await response.json();
+        setClients(data);
+      } catch (error: any) {
+        console.log(error);
+      }
+    };
+
+    fetchClients();
+  }, []);
+  return (
+    <div>
+      <h1>Client List</h1>
+      <table className="table">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Nationality</th>
+            <th>Occupation</th>
+            <th>Email</th>
+          </tr>
+        </thead>
+        <tbody>
+          {clients.map((client, index) => (
+            <tr key={index}>
+              <td>{client.name}</td>
+              <td>{client.nationality}</td>
+              <td>{client.occupation}</td>
+              <td>{client.email}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 };
 
 export default Home;
